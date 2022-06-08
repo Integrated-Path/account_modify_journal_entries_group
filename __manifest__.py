54385f0c4beb6a78e3d7fa12df9ec8a89e54075b
M-Saeb <mohammed.saeb98@hotmail.com>
2022-06-08 10:42:19 +0300

    init commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2018 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Accouting Modify Journal Entires Group",
+    "version": "14.1",
+    "license": "LGPL-3",
+    "author": "Integrated Path",
+    "website": "https://www.int-path.com",
+    "depends": ["account"],
+    "data": [
+    ],
+}
